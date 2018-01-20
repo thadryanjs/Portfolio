04390bbde4742d9c90bce5ffbf39b4b6a95e19e6
sweeney-th <sweeney.th@husky.neu.edu>
2018-01-20 13:46:01 -0500

    Added seq_to_seq, Entry class

--- ASH/ASH++/ash.cpp
+++ ASH/ASH++/ash.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>     //abs
+#include <vector>
 #include <map>
-#include "ash_utils.h"
+#include <stdlib.h>         // for abs() function
+#include "Entry.h"          // contains Entry class. Req for ash_utils
+#include "ash_utils.h"      // contains ASH functions
 
 using namespace std;
 
 
 int main()
 {
-    string seq1 = "PEPTYDE";
-    string seq2 = "PEPTIDE";
+    string x = "CDSVVVHVLKLQGAVPFVHTNVPQSMFSYDCSNPLFGQTVNPWKSSKSPGGSSGGEGALI";
+    string y = "TDATVVALLKGAGAIPLGITNCSELCMWYESSNKIYGRSNNPYDLQHIVGGSSGGEGCTL";
+    int l = 15;
+    vector<Entry> a = seq_to_seq(x,y,l);
 
-    double ans = mismatch(seq1, seq2);
-    cout << ans << endl;
-
+    for( Entry item : a) {
+        if (item.score > 4 ) {
+            cout << item.pos << "\t";
+            cout << item.seq << "\t";
+            cout << item.score << "\t";
+            cout << item.match << "\t" << endl;
+        }
+    }
 return 0;
 }
