d19050d9d871cbc43a0f1ac00d7e0c40cb32289a
sweeney-th <sweeney.th@husky.neu.edu>
2018-01-20 16:48:38 -0500

    Commenting Entry.h

--- ASH/ASH++/Entry.h
+++ ASH/ASH++/Entry.h
@@ -3,14 +3,24 @@
 
 using namespace std;
 
+// class for results of ASH
 class Entry
 {
     public:
+
+        // seq itself as kmer
         string seq;
+
+        // where in the sull seq it is located
         int pos;
+
+        // what score it is given by the algorith
         double score;
+
+        // kmer in second sequence it was compared to
         string match;
 
+        // constructor to set values from within program
         Entry(string iseq, int ipos, double iscore, string imatch)
         {
             seq = iseq;
