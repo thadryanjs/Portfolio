2363a482cdf0bd055f79e6a934fe181f629fa6d4
Thadryan <sweeney.th@husky.neu.edu>
2018-10-07 12:27:41 -0400

    better comments for example script

--- ASH/run_ash.py
+++ ASH/run_ash.py
@@ -4,7 +4,7 @@ import sys
 import ASH
 
 
-### get flagged commandline arguments
+ """   |get flagged commandline arguments|   """
 
 parser = argparse.ArgumentParser()
 
@@ -13,12 +13,11 @@ parser.add_argument("-f2", "--fasta2")
 parser.add_argument("-k, ", "--kmer")
 parser.add_argument("-o, ", "--outfile")
 
-# parse ommand line input
+# parse them
 args = parser.parse_args()
 
 
-
-### handle exceptions
+"""   |handle exceptions|   """
 
 # fileIO
 try:
@@ -38,35 +37,17 @@ except ValueError:
     sys.exit("Please enter an integer for --kmer")
 
 
+"""   |main|   """
 
-###| main |###
 ash_obj = ASH.Analysis(args.fasta1, args.fasta2, args.kmer)
 
-# get entry objects
-
-
-#ash_entries = ash_obj.get_entries()
-
-# inspect their seq attribute
-# for e in ash_entries:
-#     print(e.seq)
-
-# class Entry(object):
-#     # arguments are passed to the contructor by the seq_to_seq function
-#     def __init__(self, seq, pos, hy_score, str_score,
-#                                 hy_pct, str_pct, analog):
-#         self.seq       = seq   # the peptide
-#         self.pos       = pos   # what index is appears
-#         self.hy_score  = hy_score   # the mismatch score
-#         self.str_score = str_score  # structural mismatch
-#         self.analog    = analog     # what it was compared to
-#         self.hy_pct    = hy_pct
-#         self.str_pct   = str_pct
-
-
+# open outfile
 with open(args.outfile, "w") as outfile:
+    # write header
     outfile.write("\t".join(["seq", "pos", "hy_score", "str_score", "analog", "hy_pct", "str_pct\n"]))
+    # iterate entries in ASH report
     for e in ash_obj.get_entries():
+        # get entries attributes as strings, write them to csv 
         out_data = map(str, [e.seq, e.pos, e.hy_score, e.str_score, e.analog, e.hy_pct, e.str_pct, "\n"])
         outfile.write("\t".join(out_data))
     outfile.close()
