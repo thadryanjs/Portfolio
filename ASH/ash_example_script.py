377eab3a56f9ad51075fb4fbff2b4e5664981ab0
Thadryan <sweeney.th@husky.neu.edu>
2018-10-07 11:39:31 -0400

    update ash_example_script.py

--- ASH/ash_example_script.py
+++ ASH/ash_example_script.py
@@ -1,22 +1,25 @@
+
+### example script of a simple use case for ASH
+
 # import the class
-import ash
+import ASH
 
 # call the function on two files, select kmer size
-hiv_ash_obj = ash.Analysis("ENV_HV1MN.fasta", "ENV_HV1VI.fasta", 15)
+hiv_ash_obj = ASH.Analysis("test_data/ENV_HV1MN.fasta", "test_data/ENV_HV1VI.fasta", 15)
 
 # print alignment
-print("Aligned Sequences")
+print("Aligned Sequences:")
 print(hiv_ash_obj.aligned)
 
 hiv_entries = hiv_ash_obj.get_entries()
 
 # look at the first 15 kmers and thier score
-print("First 15 entries")
+print("\nFirst 15 entries:")
 for record in hiv_entries[:5]:
     print(record.seq, record.str_score)
 
 # conditional filtration to find distinct regions with good antigenicty
-print("Filtered Results:")
+print("\nFiltered Results:")
 for record in hiv_entries:
     if record.hy_score > 10 and record.str_score > 2:
         print(record.seq, record.hy_score, "at", record.pos)
